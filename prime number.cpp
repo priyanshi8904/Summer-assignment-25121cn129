13bc54a489cf42d4e1eee8a295feb8fe3a8eaef0
priyanshi8904 <25121cn129@glbitm.ac.in>
2026-06-03 19:24:01 +0530

    prime number

---
+++ prime number.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{int num,i,prime=1;
+printf("enter a number:");
+scanf("%d",&num);
+if(num<=1)
+{prime=0;
+}
+else {for(i=2;i<=num/2;i++){if(num%i==0)
+{prime=0;
+break;
+}
+}
+}
+if(prime)
+printf("%d is a prime number",num);
+else("%d is not a prime number",num);
+return 0;
+}
