6b7577e87f48e7abd6dcb2d8b18ff4291098dbb8
priyanshi8904 <25121cn129@glbitm.ac.in>
2026-06-03 19:37:41 +0530

    range of prime number

---
+++ range of prime number.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{int start,end,i,j,prime;
+printf("enter the starting number");
+scanf("%d",&start);
+printf("enter the ending number");
+scanf("%d",&end);
+printf("prime numbers between %d and %d are:",start,end);
+for(i=start;i<=end;i++)
+{if(i<=1)
+continue;
+
+prime=1;
+for(j=2;j<=i/2;j++)
+{if (i%j==0){prime=0;
+break;
+}
+}
+if(prime)
+printf("%d",i);
+}return 0;
+}
